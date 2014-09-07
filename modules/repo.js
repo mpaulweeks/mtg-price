505c03e26968f0c0c4e31335d2a2e50f29daf2ed
mpaulweeks <mpaulweeks@gmail.com>
2014-09-07 08:32:56 -0400

    reduce dupe code in repo via closure

--- modules/repo.js
+++ modules/repo.js
@@ -1,43 +1,45 @@
 var store = require('./store.js');
 var metadata = require('./metadata.js');
 
-var landCache = null;
-var edhCache = null;
+var makeCache = function(storeFunc){
+	return (function (){
+		var cards = null;
+		var publicMembers = {};
+		
+		publicMembers.getCards = function(callback){
+			if(cards){
+				return callback(null, cards);
+			} else {
+				storeFunc(function(err, data){
+					cards = data;
+					metadata.updateCards(cards);
+					return callback(null, cards);
+				});
+			}
+		};
+		
+		publicMembers.refresh = function(callback){
+			cards = null;
+			return publicMembers.getCards(callback);
+		};
+		
+		return publicMembers;
+	})();
+}
 
-var getLandFromFile = function(callback){
-	store.getLandFromFile(function(err, data){
-		landCache = data;
-		metadata.updateCards(landCache);
-		return callback(null, landCache);
-	});
-};
-
-var getEdhFromFile = function(callback){
-	store.getEdhFromFile(function(err, data){
-		edhCache = data;
-		metadata.updateCards(edhCache);
-		return callback(null, edhCache);
-	});
-};
+var landCache = makeCache(store.getLandFromFile);
+var edhCache = makeCache(store.getEdhFromFile);
 
 module.exports.getLand = function(callback){
-	if(landCache){
-		return callback(null, landCache);
-	} else {
-		return getLandFromFile(callback);
-	}
+	return landCache.getCards(callback);
 };
 
 module.exports.getEdh = function(callback){
-	if(edhCache){
-		return callback(null, edhCache);
-	} else {
-		return getEdhFromFile(callback);
-	}
+	return edhCache.getCards(callback);
 };
 
 module.exports.refresh = function(callback){
-	store.saveCardsToFile(function(message){
+	store.saveCardsToFile(function(err, message){
 		var count = 0;
 		var finish = function(mess){
 			message += mess;
@@ -46,10 +48,10 @@ module.exports.refresh = function(callback){
 				return callback(null, message);
 			};
 		};
-		getLandFromFile(function(cards){
+		landCache.refresh(function(err, cards){
 			return finish('loaded ' + cards.length + ' cards into landCache<br/>');
 		});
-		getEdhFromFile(function(cards){
+		edhCache.refresh(function(err, cards){
 			return finish('loaded ' + cards.length + ' cards into edhCache<br/>');
 		});
 	});
